6038436a12f13ef747441d8c56b9fe4be2ffd052
Alperen <alperentak04@gmail.com>
2026-07-13 12:18:34 +0300

    feat: pipeline yapısı ekle ve cli komutlarını sadeleştir

--- src/cli.py
+++ src/cli.py
@@ -2,14 +2,10 @@ import sys
 from pathlib import Path
 
 import click
-import torch
 
-from src.utils import set_random_seed
-from src.builders import build_evaluator, build_model, build_trainer
 from src.config import Config
-from src.dataset import load_fashion_mnist
-from src.saver import load_model, save_model
-from src.trainer import Trainer
+from src.pipeline import PipelineRunner
+from src.utils import set_random_seed
 
 
 @click.group()
@@ -35,180 +31,14 @@ def cli(ctx: click.Context, config: str):
 
 @cli.command()
 @click.pass_context
-def list_models(ctx):
-    """
-    model yapılandırmalarını listeler
-    """
-    print()
-    for model in ctx.obj.get("config").conf["models"]:
-        print("-" * 30)
-        print(f"name: {model['name']}")
-        print(f"type: {model['type']}")
-        print("args:")
-        print("\n".join(f"\t{key}: {value}" for key, value in model["args"].items()))
-        print("-" * 30)
-    print()
-
-
-@cli.command()
-@click.pass_context
-def list_trainers(ctx):
-    """
-    trainer yapılandırmalarını listeler
-    """
-    print()
-    for trainer in ctx.obj.get("config").conf["trainers"]:
-        print("-" * 30)
-        print(f"name: {trainer['name']}")
-        print(f"epochs: {trainer['epochs']}")
-        print(f"device: {trainer['device']}")
-
-        print("loss_fn:")
-        print(f"\ttype: {trainer['loss_fn']['type']}")
-        print("\targs:")
-        print(
-            "\n".join(
-                f"\t\t{key}: {value}"
-                for key, value in trainer["loss_fn"]["args"].items()
-            )
-        )
-
-        print("optimizer:")
-        print(f"\ttype: {trainer['optimizer']['type']}")
-        print("\targs:")
-        print(
-            "\n".join(
-                f"\t\t{key}: {value}"
-                for key, value in trainer["optimizer"]["args"].items()
-            )
-        )
-        print("-" * 30)
-    print()
-
-
-@cli.command()
-@click.pass_context
-def list_evaluators(ctx):
-    """
-    evaluator yapılandırmalarını listeler
-    """
-    print()
-    for evaluator in ctx.obj.get("config").conf["evaluators"]:
-        print("-" * 30)
-        print(f"name: {evaluator['name']}")
-        print(f"device: {evaluator['device']}")
-
-        print("loss_fn:")
-        print(f"\ttype: {evaluator['loss_fn']['type']}")
-        print("\targs:")
-        print(
-            "\n".join(
-                f"\t\t{key}: {value}"
-                for key, value in evaluator["loss_fn"]["args"].items()
-            )
-        )
-        print("-" * 30)
-    print()
-
-
-@cli.command()
-@click.option("-m", "--model-name", required=True, help="eğitilecek model ismi")
-@click.option("-t", "--trainer-name", required=True, help="kullanılacak trainer ismi")
-@click.option("-n", "--saved-name", required=True, help="kaydedilen modelin ismi")
-@click.pass_context
-def train_model(ctx, model_name: str, trainer_name: str, saved_name: str):
-    """
-    trainer ile modeli eğitir ve kaydeder
-    """
+def run_pipeline(ctx, pipeline_config_path: str = "./pipeline.toml"):
     conf: dict = ctx.obj.get("config").conf
 
-    models_conf = conf["models"]
-    trainers_conf = conf["trainers"]
+    if not Path(pipeline_config_path).exists():
+        sys.exit(f"pipeline config dosyası bulunamadı!\n{pipeline_config_path}")
 
-    model_save_dir = conf["model_save_dir"]
-    model_save_path = Path(model_save_dir) / f"{saved_name}.pt"
-
-    if model_save_path.exists():
-        sys.exit("bu isimde bir model mevcut!")
-
-    model_exist = False
-    selected_model: dict
-    for model in models_conf:
-        if model["name"] == model_name:
-            selected_model = model
-            model_exist = True
-            break
-
-    if not model_exist:
-        sys.exit(f"{model_name} bulunamadı!")
-
-    trainer_exist = False
-    selected_trainer: dict
-    for trainer in trainers_conf:
-        if trainer["name"] == trainer_name:
-            selected_trainer = trainer
-            trainer_exist = True
-            break
-
-    if not trainer_exist:
-        sys.exit(f"{trainer_name} bulunamadı!")
-
-    model: torch.nn.Module = build_model(conf=selected_model)
-    trainer: Trainer = build_trainer(conf=selected_trainer, model=model)
-
-    train_data_loader, _, _ = load_fashion_mnist()
-
-    trainer.train_model(model=model, train_data_loader=train_data_loader)
-
-    save_model(
-        model=model,
-        save_dir=conf["model_save_dir"],
-        name=saved_name,
-        model_parameters=selected_model,
-        trainer_parameters=selected_trainer,
+    pipeline_runner = PipelineRunner(
+        pipeline_config_path=pipeline_config_path,
+        config=conf,
     )
-
-    click.echo(f"eğitilen model {conf['model_save_dir']} adresine kaydedildi")
-
-
-@cli.command()
-@click.option(
-    "-m", "--model-name", required=True, help="değerlendirilecek modelin ismi"
-)
-@click.option(
-    "-e", "--evaluator-name", required=True, help="kullanılacak evaluator ismi"
-)
-@click.pass_context
-def eval_model(ctx, model_name: str, evaluator_name: str):
-    """
-    eğitilmiş bir modeli değerlendirir ve metrik değerlerini kaydeder
-    """
-    conf: dict = ctx.obj.get("config").conf
-    evaluators_conf = conf["evaluators"]
-    model_save_dir = conf["model_save_dir"]
-
-    model: torch.nn.Module = load_model(model_name, model_save_dir)
-
-    _, test_data_loader, _ = load_fashion_mnist()
-
-    evaluator_exist = False
-    selected_evaluator: dict
-    for evaluator in evaluators_conf:
-        if evaluator["name"] == evaluator_name:
-            selected_evaluator = evaluator
-            evaluator_exist = True
-            break
-
-    if not evaluator_exist:
-        sys.exit(f"{evaluator_name} bulunamadı!")
-
-    evaluator = build_evaluator(conf=selected_evaluator)
-
-    evaluator.eval_model(
-        model=model,
-        test_data_loader=test_data_loader,
-        model_name=model_name,
-        eval_results_path=conf["eval_results_path"],
-    )
-
-    click.echo(f"metrik değerleri {conf['eval_results_path']} dosyasına kaydedildi")
+    pipeline_runner.run_pipelines()
---
+++ src/pipeline.py
@@ -0,0 +1,112 @@
+import sys
+import tomllib
+
+import click
+
+from src.builders import build_evaluator, build_model, build_trainer
+from src.dataset import load_fashion_mnist
+from src.saver import is_exist, save_model
+
+
+class PipelineRunner:
+    def __init__(
+        self,
+        pipeline_config_path: str,
+        config: dict,
+    ):
+        self.pipelines: dict = self._read_conf(pipeline_config_path)
+        self.config = config
+
+    def _read_conf(self, config_path: str) -> dict:
+        try:
+            with open(config_path, "rb") as f:
+                config = tomllib.load(f)
+        except Exception as e:
+            sys.exit(f"pipeline config dosyası okunamadı!\n{e}")
+
+        if "pipelines" not in config or config["pipelines"] is None:
+            sys.exit("pipeline config: pipelines listesi bulunamadı!")
+        if not isinstance(config["pipelines"], list):
+            sys.exit("pipeline config: pipelines değeri list olmalıdır!")
+        pipeline_keys = {
+            "name": str,
+            "model": str,
+            "trainer": str,
+        }
+        unique_pipeline_names = list()
+        for pipeline in config["pipelines"]:
+            for key, value in pipeline_keys.items():
+                if key not in pipeline or pipeline[key] is None:
+                    sys.exit(f"pipeline config: pipeline {key} alanı boş bırakılamaz!")
+                if not isinstance(pipeline[key], value):
+                    sys.exit(
+                        f"pipeline config: pipeline {key} değeri {value} olmalıdır!"
+                    )
+            if pipeline["name"] in unique_pipeline_names:
+                sys.exit("pipeline config: pipeline name değeri özgün olmalıdır!")
+            unique_pipeline_names.append(pipeline["name"])
+
+        return config["pipelines"]
+
+    def run_pipelines(self):
+        models_conf = self.config["models"]
+        trainers_conf = self.config["trainers"]
+
+        model_save_dir = self.config["model_save_dir"]
+
+        for pipeline in self.pipelines:
+            model_name = pipeline["model"]
+            trainer_name = pipeline["trainer"]
+
+            model_exist = False
+            selected_model_conf: dict
+            for model_conf in models_conf:
+                if model_conf["name"] == model_name:
+                    selected_model_conf = model_conf
+                    model_exist = True
+                    break
+
+            if not model_exist:
+                sys.exit(f"{model_name} bulunamadı!")
+
+            trainer_exist = False
+            selected_trainer_conf: dict
+            for trainer_conf in trainers_conf:
+                if trainer_conf["name"] == trainer_name:
+                    selected_trainer_conf = trainer_conf
+                    trainer_exist = True
+                    break
+
+            if not trainer_exist:
+                sys.exit(f"{trainer_name} bulunamadı!")
+
+            if is_exist(
+                save_dir=model_save_dir,
+                model_conf=selected_model_conf,
+                trainer_conf=selected_trainer_conf,
+            ):
+                click.echo(f"bu model zaten eğitilmiş: {pipeline['name']}")
+                continue
+
+            click.echo(f"model eğitiliyor: {pipeline['name']}")
+            model = build_model(selected_model_conf)
+            trainer = build_trainer(conf=selected_trainer_conf, model=model)
+            evaluator = build_evaluator(selected_trainer_conf)
+            train_data_loader, test_data_loader, _ = load_fashion_mnist()
+
+            trainer.train_model(model=model, train_data_loader=train_data_loader)
+
+            save_model(
+                model=model,
+                save_dir=model_save_dir,
+                name=pipeline["name"],
+                model_parameters=selected_model_conf,
+                trainer_parameters=selected_trainer_conf,
+            )
+
+            evaluator.eval_model(
+                model=model,
+                test_data_loader=test_data_loader,
+                model_name=pipeline["name"],
+                eval_results_path=self.config["eval_results_path"],
+            )
