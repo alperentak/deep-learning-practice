3596a8eec540852e40c5c6285ed58bf19e52cae3
Alperen <alperentak04@gmail.com>
2026-07-15 17:32:09 +0300

    feat: folder yapısına uygun dataset oluşturan builder ekle

--- src/builders.py
+++ src/builders.py
@@ -1,7 +1,12 @@
 import sys
+from pathlib import Path
 
+import click
 import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
 
+from src.dataset import ImageDataset
 from src.evaluator import Evaluator
 from src.models import BaselineModel, CNNModel
 from src.trainer import Trainer
@@ -94,3 +99,53 @@ def build_evaluator(conf: dict) -> Evaluator:
     )
 
     return evaluator
+
+
+def build_dataset(conf: dict) -> Dataset:
+    transforms_conf: list = conf["transforms"]
+    transforms_map = {
+        "Resize": transforms.Resize,
+        "RandomHorizontalFlip": transforms.RandomHorizontalFlip,
+        "ToTensor": transforms.ToTensor,
+    }
+
+    transforms_list = list()
+    for t in transforms_conf:
+        transform_class = transforms_map.get(t["name"])
+        if transform_class is None:
+            click.echo(f"datasets: {t['name']} bulunamadı!")
+            continue
+        transform_args = t.get("args", [])
+        try:
+            transforms_list.append(transform_class(*transform_args))
+        except Exception as e:
+            sys.exit(f"transform {transform_class} eklenemedi!\n{e}")
+
+    if len(transforms_list) > 0:
+        transform = transforms.Compose(transforms_list)
+    else:
+        transform = None
+
+    data_dir = Path(conf["data_dir"])
+    class_names = sorted(entry.name for entry in data_dir.iterdir() if entry.is_dir())
+
+    if not class_names:
+        sys.exit(f"{data_dir}: class klasörleri bulunamadı!")
+
+    classes = {class_name: i for i, class_name in enumerate(class_names)}
+
+    data_list = list()
+    for class_name, index in classes.items():
+        class_dir = data_dir / class_name
+        image_paths = [Path(image_path) for image_path in class_dir.iterdir()]
+
+        image_extensions = {".png", ".jpg", ".jpeg"}
+        for img_path in image_paths:
+            if img_path.is_file() and img_path.suffix.lower() in image_extensions:
+                data_list.append({"path": img_path, "label": index})
+
+    image_dataset = ImageDataset(
+        classes=classes, data_list=data_list, transform=transform
+    )
+
+    return image_dataset
