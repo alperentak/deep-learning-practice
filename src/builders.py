7fdf23b4ff8b6d876fb70c1b48b427754c972ea9
Alperen <alperentak04@gmail.com>
2026-07-16 22:17:13 +0300

    feat: dataset builder yapısına train/test split özelliği ekle

--- src/builders.py
+++ src/builders.py
@@ -1,3 +1,4 @@
+import random
 import sys
 from pathlib import Path
 
@@ -101,30 +102,38 @@ def build_evaluator(conf: dict) -> Evaluator:
     return evaluator
 
 
-def build_dataset(conf: dict) -> Dataset:
-    transforms_conf: list = conf["transforms"]
+def build_dataset(conf: dict) -> tuple[Dataset, Dataset]:
     transforms_map = {
         "Resize": transforms.Resize,
         "RandomHorizontalFlip": transforms.RandomHorizontalFlip,
         "ToTensor": transforms.ToTensor,
     }
 
-    transforms_list = list()
-    for t in transforms_conf:
-        transform_class = transforms_map.get(t["name"])
-        if transform_class is None:
-            click.echo(f"datasets: {t['name']} bulunamadı!")
-            continue
-        transform_args = t.get("args", [])
-        try:
-            transforms_list.append(transform_class(*transform_args))
-        except Exception as e:
-            sys.exit(f"transform {transform_class} eklenemedi!\n{e}")
+    transforms_dict = {
+        "train": list(),
+        "test": list(),
+    }
+    transform = {
+        "train": None,
+        "test": None,
+    }
+    for split in {"train", "test"}:
+        transforms_conf = conf[split]["transforms"]
+        for t in transforms_conf:
+            transform_class = transforms_map.get(t["name"])
+            if transform_class is None:
+                click.echo(f"datasets: {t['name']} bulunamadı!")
+                continue
+            transform_args = t.get("args", [])
+            try:
+                transforms_dict[split].append(transform_class(*transform_args))
+            except Exception as e:
+                sys.exit(f"transform {transform_class} eklenemedi!\n{e}")
 
-    if len(transforms_list) > 0:
-        transform = transforms.Compose(transforms_list)
-    else:
-        transform = None
+        if len(transforms_dict[split]) > 0:
+            transform[split] = transforms.Compose(transforms_dict[split])
+        else:
+            transform[split] = None
 
     data_dir = Path(conf["data_dir"])
     class_names = sorted(entry.name for entry in data_dir.iterdir() if entry.is_dir())
@@ -134,18 +143,33 @@ def build_dataset(conf: dict) -> Dataset:
 
     classes = {class_name: i for i, class_name in enumerate(class_names)}
 
-    data_list = list()
+    train_list = list()
+    test_list = list()
+
+    image_extensions = {".png", ".jpg", ".jpeg"}
+    train_test_split = conf["train_test_split"]
     for class_name, index in classes.items():
         class_dir = data_dir / class_name
-        image_paths = [Path(image_path) for image_path in class_dir.iterdir()]
+        image_paths = [
+            Path(image_path)
+            for image_path in class_dir.iterdir()
+            if image_path.is_file() and image_path.suffix.lower() in image_extensions
+        ]
 
-        image_extensions = {".png", ".jpg", ".jpeg"}
-        for img_path in image_paths:
-            if img_path.is_file() and img_path.suffix.lower() in image_extensions:
-                data_list.append({"path": img_path, "label": index})
+        random.shuffle(image_paths)
 
-    image_dataset = ImageDataset(
-        classes=classes, data_list=data_list, transform=transform
+        split_index = int(len(image_paths) * train_test_split)
+
+        for img_path in image_paths[:split_index]:
+            train_list.append({"path": img_path, "label": index})
+        for img_path in image_paths[split_index:]:
+            test_list.append({"path": img_path, "label": index})
+
+    train_dataset = ImageDataset(
+        classes=classes, data_list=train_list, transform=transform["train"]
+    )
+    test_dataset = ImageDataset(
+        classes=classes, data_list=test_list, transform=transform["test"]
     )
 
-    return image_dataset
+    return train_dataset, test_dataset
